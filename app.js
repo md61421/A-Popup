126fc19801273742f731559c0b804d51c859f960
Md Sahil <md61421@gmail.com>
2024-01-14 12:21:55 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let popup = document.querySelector("#popup");
+
+const openPopup = () => {
+    popup.classList.add("open-popup");
+}
+
+const closePopup = () => {
+    popup.classList.remove("open-popup");
+}
